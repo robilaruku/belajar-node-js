40ddf384f9c62f7b257da972817da237938d1533
robi <robidream20@gmail.com>
2022-09-19 11:43:55 +0700

    add path

---
+++ basic/path.mjs
@@ -0,0 +1,9 @@
+import path from "path"
+
+const file = "/basic/async.js"
+
+console.info(path.basename(file))
+console.info(path.dirname(file))
+console.info(path.extname(file))
+console.info(path.parse(file))
+console.info(path.sep)
